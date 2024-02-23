01f34ea42dd0f618bbd675d1c088b5a1df2b2846
lanterndb <darrett.johnson@yahoo.com>
2024-02-23 07:54:35 -0500

    update Player

--- App/Game/game.js
+++ App/Game/game.js
@@ -1,19 +1,39 @@
 import { Player } from "./player.js"
 
 export function initGame(interaction) {
-      
+  
+  
+  const STARTING_HEALTH = 100
+  const STARTING_SHIELDS = 3
+
   const player_one = new Player({
     id: interaction.user.id, 
-    name: interaction.user.globalName
+    username: interaction.user.globalName,
+    health: STARTING_HEALTH,
+    shields: STARTING_SHIELDS
   })
 
   const player_two = new Player({
     id: interaction.options.getUser('user').id,
-    name: interaction.options.getUser('user').globalName
+    username: interaction.options.getUser('user').globalName,
+    health: STARTING_HEALTH,
+    shields: STARTING_SHIELDS
   })
 
   interaction.reply({
-    content: `${interaction}`
+    content: `
+      player_one: {
+        id: ${player_one.id},
+        username: ${player_one.username},
+        health: ${player_one.health},
+        shields: ${player_one.shields},
+      },
+      player_two: {
+        id: ${player_two.id},
+        username: ${player_two.username},
+        health: ${player_two.health},
+        shields: ${player_two.shields},
+      },
+    `
   })
-  
 }
--- App/Game/player.js
+++ App/Game/player.js
@@ -1,37 +1,20 @@
 export class Player {
   
-  constructor({ id: id, username: username }) {
+  constructor({ id, username, health, shields }) {
     this.id = id
     this.username = username
-    this.health = 100
-    this.shields = 3
+    this.health = health
+    this.shields = shields
     this.damage_modifier = 1.0
     this.damage_reduction_modifier = 1.0
     this.status = []
   }
 
-
-
-  getDamage(value) {
-    return Math.round(value * this.damage_modifier)
-  }
-
-  addHealth(value) {
-    return this.health += value
-  }
-
-  removeHealth(value) {
-    const damage = Math.round(value * this.damage_reduction_modifier)
-    if (this.health - damage <= 0) return this.health = 0  
-    return this.health -= damage
-  }
-
+  getDamage(value) {}
+  addHealth(value) {}
+  removeHealth(value) {}
   addShield(value = 1) {}
   removeShield(value = 1) {}
   addStatus() {}
   getStatus() {}
-
-
-
-
 }
